55e3b0342d75a04fe3b93cdc02106e8d74e3ee2e
Luximus <thomaslipman10@gmail.com>
2021-11-16 14:20:05 +0100

    Fix DuckDuckGo homepage

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
 const url = window.location.href;
-const baseDdgUrl = 'https://duckduckgo.com/';
 
 if (url.includes('www.bing.com')) {
   try {
     const searchterm = url.split('?q=')[1].split('&')[0];
-    window.location.replace(baseDdgUrl + '?q=' + searchterm);
+    window.location.replace('https://duckduckgo.com/?q=' + searchterm);
   } catch {
-    window.location.replace(baseDdgUrl);
+    window.location.replace('https://start.duckduckgo.com/');
   }
 }
